43f3dedea8cf98828aec975bbc1b298e0f6ef3a6
YoussefAbdallah <youssefabdullah@hotmail.com>
2022-05-02 13:31:26 +0200

    Updated ProductItemDetailComponent.

--- src/app/components/product-item-detail/product-item-detail.component.ts
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product } from 'src/app/models/product.model';
+import { CartService } from 'src/app/cart.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-item-detail',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductItemDetailComponent implements OnInit {
 
-  constructor() { }
+  quantityOptions = Array.from({length: 10}, (_, i) => i + 1);
+  selectedQuantity: number = 1;
+  id: number = 0;
+  product: Product = {
+    id: 0,
+    name: '',
+    price: 0,
+    url: '',
+    description: ''
+  };
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private cartService: CartService,
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      let idStr = params.get('id');
+      this.id = idStr ? +idStr : 0;
+    });
+
+    this.productService.getProducts().subscribe(productList => {
+      let productIdx = productList.findIndex(product => {
+        return product.id === this.id;
+      });
+      this.product = (productIdx !== -1)
+        ? productList[productIdx]
+        : this.product;
+    });
   }
 
+  addItemToCart(product: Product, quantity: number): void {
+    this.cartService.addOrUpdateCartItem
+      ({...product, quantity: quantity});
+    alert(`Product ${product.name} was added to cart successfully!`);
+  }
 }
